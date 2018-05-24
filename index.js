05eb4d432da244993997f88e05562cf0a2d2939a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 18:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ function produceDrivingRange(range){
 }
 
 function produceTipCalculator(tipPercentage){
-  return function(total){
-    return total * tipPercentage;
-  }
+  return total => total * tipPercentage;
 }
 
 // function createDriver(){
