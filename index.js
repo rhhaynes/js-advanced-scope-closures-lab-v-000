252fbf95d242285e4f24a835657a98c2489f0369
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-24 18:05:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function produceTipCalculator(){
   return function(){
   }
 }
+
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor {
+      this.id = ++driverId;
+    }
+  }
+}
